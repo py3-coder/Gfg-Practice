e3367fc3baa0c0f8c3cd4e9ba619a96ab7d56d89
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-04-23 21:47:27 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -32,78 +32,57 @@ public class Main{
 
 
 // User function Template for Java
-class DistjointSet{
-    List<Integer> size = new ArrayList<>();
-    List<Integer> parent =new ArrayList<>();
-    
-    public DistjointSet(int n){
-        for(int i=0;i<=n;i++){
-            size.add(1);
-            parent.add(i);
-        }
-    }
-    
-    public int findPar(int node){
-        if(parent.get(node)==node){
-            return node;
-        }
-        int par =findPar(parent.get(node));
-        parent.set(node,par);
-        return parent.get(node);
-    }
-    
-    public void unionBySize(int u,int v){
-        int utp_u =findPar(u);
-        int utp_v =findPar(v);
-        
-        if(utp_u == utp_v) return;
-        
-        if(size.get(utp_u)>size.get(utp_v)){
-            parent.set(utp_v,utp_u);
-            size.set(utp_u,size.get(utp_u)+size.get(utp_v));
-        }else{
-            parent.set(utp_u,utp_v);
-            size.set(utp_v,size.get(utp_u)+size.get(utp_v));
-        }
-    }
-}
+
 class Solution{
 	static int spanningTree(int V, int E, int edges[][]){
-	    // Code Here.
-	    
-	    List<Triple> edge = new ArrayList<>();
-	    
-	    for(int i=0;i<edges.length;i++){
-	        edge.add(new Triple(edges[i][2],edges[i][0],edges[i][1]));
+
+	    // Let's Do it with Prism Algo.
+	    ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<>();
+	    for(int i=0;i<V;i++){
+	        adj.add(new ArrayList<>());
 	    }
-	    //Collections.sort(edge);
-	    Collections.sort(edge, (a, b) -> (a.wt-b.wt));
 	    
-	    int minWt=0;
-	    DistjointSet ds = new DistjointSet(V);
-	    for(Triple curr : edge){
-	        int wt =curr.wt;
-	        int u =curr.u;
-	        int v =curr.v;
+	    for(int[] edge : edges){
+	        int u = edge[0];
+	        int v =edge[1];
+	        int wt =edge[2];
 	        
-	        if(ds.findPar(u)!=ds.findPar(v)){
-	            minWt+=wt;
-	            ds.unionBySize(u,v);
+	        adj.get(u).add(new ArrayList<>(Arrays.asList(v,wt)));
+	        adj.get(v).add(new ArrayList<>(Arrays.asList(u,wt)));
+	    }
+	    
+	    int[] vis =new int[V];
+	    PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->(a.dist-b.dist));
+	    pq.offer(new Pair(0,0));
+	    int sum=0;
+	    while(!pq.isEmpty()){
+	        Pair pp = pq.poll();
+	        int node =pp.node;
+	        int dist =pp.dist;
+	        if(vis[node]==1) continue;
+	        
+	        vis[node]=1;
+	        sum+=dist;
+	        
+	        for(ArrayList<Integer> curr : adj.get(node)){
+	            int currNode =curr.get(0);
+	            int currWt =curr.get(1);
+	            
+	            if(vis[currNode]!=1){
+	                pq.offer(new Pair(currNode,currWt));
+	            }
 	        }
-	        
 	    }
-	    return minWt;
-	    
+	    return sum;
 	}
-	static class Triple{
-	    int wt;
-	    int u;
-	    int v;
-	    
-	    Triple(int _wt,int _u,int _v){
-	        this.wt =_wt;
-	        this.u =_u;
-	        this.v=_v;
-	    }
-	}
+}
+class Pair{
+    int node;
+    int dist;
+    
+    
+    Pair(int _n,int _d){
+        this.node =_n;
+        this.dist=_d;
+    }
 }
