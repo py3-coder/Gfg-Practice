dbd5e0e665cae85753ed02f1185cd4026f4ecb95
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-04-23 18:46:31 +0530

    Updated Solution - GfG to GitHub

--- Medium/Floyd Warshall/floyd-warshall.java
+++ Medium/Floyd Warshall/floyd-warshall.java
@@ -41,36 +41,29 @@ class Solution
     public void shortest_distance(int[][] matrix)
     {
         // Code here 
-        
-        // TC : O()
-        int n =matrix.length;
-        
+        int n = matrix.length;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==-1){
-                    matrix[i][j]=(int)1e9;
-                }
-                if(i==j){
-                    matrix[i][j]=0;
+                if(matrix[i][j] ==-1){
+                    matrix[i][j] = (int)1e9;
                 }
             }
         }
         
-        for(int via=0;via<n;via++){
+        for(int k=0;k<n;k++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
-                    matrix[i][j] =Math.min(matrix[i][j],matrix[i][via]+matrix[via][j]);
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i][k]+matrix[k][j]);
                 }
             }
         }
         
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==(int)1e9){
-                    matrix[i][j]=-1;
+                if(matrix[i][j] ==(int)1e9){
+                    matrix[i][j] = -1;
                 }
             }
         }
-        
     }
 }
