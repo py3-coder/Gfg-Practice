886352419c51b2d69662b10aba5aa5072aab5e2d
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-11 17:44:32 +0530

    Updated Solution - GfG to GitHub

--- Medium/Subset Sum Problem/subset-sum-problem.java
+++ Medium/Subset Sum Problem/subset-sum-problem.java
@@ -34,47 +34,88 @@ class GFG
 //User function Template for Java
 
 class Solution{
+
     static int[][] memo = new int[101][100001];
     static Boolean isSubsetSum(int N, int arr[], int sum){
-        // Lets Play With Recursion :::
-        //1. Recursion Call ::
-        //return solveRec(N,arr,sum);
-        //2.Memo Call ::
-        Arrays.stream(memo).forEach(a -> Arrays.fill(a, -1));
-        return solvememo(N,arr,sum)==1?true:false;
+        // code here
         
+        // Recursion :-
+        // TC :-  O(2^n)
+        // SC :- O(n) -- Aux stack
+        //return solveRec(0,N,arr,sum);
+        
+        //Memoization
+        // TC : O(n*target)
+        // SC : O(n*target) + O(n) -- Aux stack--
+        //.stream(memo).forEach(a -> Arrays.fill(a, -1));
+        //return solveMemo(0,N,arr,sum)==1?true:false;
+        
+        
+        // Tabulation
+        //TC : O(n*sum)
+        //SC : O(n*sum)
+        return solveTab(N,arr,sum)==1?true:false;
         
     }
-    public static boolean solveRec(int n,int arr[],int target){
-        //Base ::
+    static boolean solveRec(int i,int n,int[] arr, int target){
+        //Base Case ::
+        if(i>=n) return false;
         if(target==0) return true;
-        if(n==0) return false;
         
-        if(arr[n-1]>target){
-            //not possible to pick --> Not pick Case ::
-            return solveRec(n-1,arr,target);
-        }else{
-            //pick and not pick case::
-            return solveRec(n-1,arr,target-arr[n-1]) || solveRec(n-1,arr,target);
+        // Choice Diagram:-
+  
+        if(arr[i]<=target){
+            // pick case
+            // notpick case
+            return (solveRec(i+1,n,arr,target-arr[i]) || solveRec(i+1,n,arr,target)) ;
         }
+        //not pick since arr[i]> target::
+        return solveRec(i+1,n,arr,target);
     }
-    public static int solvememo(int n,int arr[],int target){
-        //Base ::
+    
+    //Memoizatiom - Bottom up 
+    static int solveMemo(int i,int n,int[] arr, int target){
+        //Base Case ::
+        if(i>=n) return 0;
         if(target==0) return 1;
-        if(n==0) return 0;
         
-        //check pre-camputed ?
-        if(memo[n][target]!=-1){
-            return memo[n][target];
+        if(memo[i][target]!=-1){
+            return memo[i][target];
         }
-        if(arr[n-1]>target){
-            //not possible to pick --> Not pick Case ::
-            return memo[n][target]=solvememo(n-1,arr,target);
-        }else{
-            //pick and not pick case::
-            return memo[n][target]=Math.max(solvememo(n-1,arr,target-arr[n-1]),solvememo(n-1,arr,target));
+        // Choice Diagram:-
+        if(arr[i]<=target){
+            // pick case
+            // notpick case
+            return memo[i][target]=Math.max(solveMemo(i+1,n,arr,target-arr[i]) , solveMemo(i+1,n,arr,target)) ;
         }
+        //not pick since arr[i]> target::
+        return memo[i][target]=solveMemo(i+1,n,arr,target);
     }
-    
-    
+    // reverse of recursion :- call
+    static int solveTab(int n,int[]arr ,int target){
+        int[][] tab = new int[n+1][target+1];
+        // Base Case ::-
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<target+1;j++){
+                if(i==0){
+                    tab[i][j]=0;
+                }
+                if(j==0){
+                    tab[i][j]=1;
+                }
+            }
+        }
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<target+1;j++){
+                
+                if(arr[i-1]<=j){
+                    tab[i][j] = Math.max(tab[i-1][j-arr[i-1]],tab[i-1][j]);
+                }else{
+                    tab[i][j] =tab[i-1][j];
+                }
+            }
+        }
+        return tab[n][target];
+    }
 }
