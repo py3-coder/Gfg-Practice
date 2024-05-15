f4b7cee1e8f5c92c564172cfff8d05c79d155977
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-15 20:18:27 +0530

    Updated Solution - GfG to GitHub

--- Medium/Perfect Sum Problem/perfect-sum-problem.java
+++ Medium/Perfect Sum Problem/perfect-sum-problem.java
@@ -25,24 +25,29 @@ class GfG
 // } Driver Code Ends
 
 
+
+
 class Solution{
     static int[][] memo;
     static int mod =1000000007;
 	public int perfectSum(int arr[],int n, int sum) 
 	{ 
-	   // memo = new int[n+1][sum+1];
-	   // Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
-	   // return solveRec(arr,n,sum);
+	    memo = new int[n+1][sum+1];
+	    Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
+	    return solveRec(arr,n,sum);
 	   
 	   //Tabulation
-	   return solveTab(arr,n,sum);
+	  // return solveTab(arr,n,sum);
+	   
+	   //optimisation
+	   //return solveOptimise(arr,n,sum);
 	    
 	} 
 	public int solveRec(int[] arr,int n, int sum){
-	    if(sum==0){
-	        return 1;
+	    if(n==0){
+	        if(sum==0) return 1;
+	        return 0;
 	    }
-	    if(n<=0) return 0;
 	    
 	    if(memo[n][sum]!=-1){
 	        return memo[n][sum];
@@ -78,5 +83,28 @@ class Solution{
 	        }
 	    }
 	    return tab[n][sum];
+	}
+	public int solveOptimise(int[] arr,int n,int sum){
+	    int[] curr = new int[sum+1];
+	    int[] prev = new int[sum+1];
+	    
+	    curr[0]=1;
+	    prev[0]=1;
+	    
+	    for(int i=1;i<=n;i++){
+	        for(int j=0;j<=sum;j++){
+	            if(arr[i-1]>j){
+	                curr[j] = prev[j];
+	            }else{
+	                curr[j] = prev[j-arr[i-1]]%mod + prev[j]%mod;
+	                curr[j]%=mod;
+	            }
+	        }
+	        
+	        for(int k=0;k<prev.length;k++){
+	            prev[k]=curr[k];
+	        }
+	    }
+	    return curr[sum];
 	}
 }
