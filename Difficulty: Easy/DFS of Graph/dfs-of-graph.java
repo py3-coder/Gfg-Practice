1c8e32dee2ce9078fb4e62d04654e9fdc5072d96
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-11-17 12:35:52 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(adj);
+            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> res = new ArrayList<>();
+        int len = adj.size();
+        int[] vis = new int[len];
+        
+        dfs(0,vis,adj,res);
+        
+        return res ;
+    }
+    
+    public void dfs(int node , int[] vis , ArrayList<ArrayList<Integer>> adj , ArrayList<Integer> res){
+        vis[node]=1;
+        res.add(node);
+        
+        for(int currNode : adj.get(node)){
+            if(vis[currNode]!=1){
+                dfs(currNode , vis ,adj ,res);
+            }
+        }
+    }
+}
