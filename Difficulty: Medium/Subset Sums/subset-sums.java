03159e8a372f6d01c7d8dd305cbaddfac13ba424
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2025-02-19 10:48:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sums/subset-sums.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            ArrayList<Integer> result = new Solution().subsetSums(arr);
+            Collections.sort(result);
+            // Printing the result in space-separated format
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0) System.out.print(" ");
+                System.out.print(result.get(i));
+            }
+            System.out.println();    // Print new line after each test case
+            System.out.println("~"); // Print new line after each test case
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java//User function Template for Java
+class Solution {
+    public ArrayList<Integer> subsetSums(int[] arr) {
+        // code here
+        ArrayList<Integer> res = new ArrayList<>();
+        solve(0,arr,0,res);
+        return res;
+    }
+    public void solve(int indx,int[] arr , int sum,ArrayList<Integer> res){
+        //base case :
+        if(indx == arr.length){
+            res.add(sum);
+            return ;
+        }
+        int pick = sum+arr[indx];
+        solve(indx+1,arr,pick,res);
+        int notpick = sum;
+        solve(indx+1,arr,notpick,res);
+    }
+}
