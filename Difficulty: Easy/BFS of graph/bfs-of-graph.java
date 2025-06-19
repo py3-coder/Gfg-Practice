2ffeb69f5caf20ef2b8efe18b3662949831f255f
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2025-06-19 21:11:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,73 +1,24 @@
-//{ Driver Code Starts
-import java.util.*;
-
-// Driver code
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int testcases = sc.nextInt(); // Taking number of test cases as input
-
-        while (testcases-- > 0) {
-            int V = sc.nextInt(); // Number of vertices
-            int E = sc.nextInt(); // Number of edges
-
-            // Initialize adjacency list
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) {
-                adj.add(new ArrayList<>()); // Create a new list for each vertex
-            }
-
-            // Add edges to the adjacency list
-            for (int i = 0; i < E; i++) {
-                int u = sc.nextInt();
-                int v = sc.nextInt();
-                adj.get(u).add(v); // Adding edge u -> v
-                adj.get(v).add(u); // Adding edge v -> u (undirected graph)
-            }
-
-            // Create Solution object and call bfsOfGraph
-            Solution obj = new Solution();
-            ArrayList<Integer> result = obj.bfsOfGraph(V, adj);
-
-            // Print the result
-            for (int node : result) {
-                System.out.print(node + " ");
-            }
-            System.out.println();
-        }
-
-        sc.close(); // Close the scanner
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
-    // Function to return Breadth First Traversal of given graph.
-    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+    // Function to return Breadth First Search Traversal of given graph.
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
         // code here
-        ArrayList<Integer> res = new ArrayList<>();
         int n = adj.size();
-        int[] vis =new int[n];
-        
+        int[] vis = new int[n];
         Queue<Integer> que = new LinkedList<>();
-        que.offer(0);
         vis[0]=1;
+        que.offer(0);
+        ArrayList<Integer> res=new ArrayList<>();
+        
         while(!que.isEmpty()){
             int node = que.poll();
-           // System.out.println(node);
             res.add(node);
-            
-            for(int currNode : adj.get(node)){
-                if(vis[currNode]!=1){
-                    que.offer(currNode);
-                    vis[currNode]=1;
+            for(int ngbr : adj.get(node)){
+                if(vis[ngbr]!=1){
+                    que.offer(ngbr);
+                    vis[ngbr]=1;
                 }
             }
         }
-        
         return res;
     }
 }
