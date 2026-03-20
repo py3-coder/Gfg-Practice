e085c5dd1935fd50771af52dd7ae108ed655825e
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2026-03-20 11:53:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
@@ -1,33 +1,34 @@
 class Solution {
     public int[] dijkstra(int V, int[][] edges, int src) {
-        // code here
-        int[] dist= new int[V];
-        Arrays.fill(dist, 100001);
-        dist[src] = 0;
+        int[] dist = new int[V];
+        Arrays.fill(dist,100001);
         List<List<int[]>> adj = new ArrayList<>();
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
+        
         for(int[] edge : edges){
-            int u=edge[0],v=edge[1],d=edge[2];
-            adj.get(u).add(new int[]{v,d});
+            int u = edge[0];
+            int v = edge[1];
+            int d = edge[2];        
+            adj.get(u).add(new int[]{v, d});
+            adj.get(v).add(new int[]{u, d});
         }
-        
+        dist[src]=0;
+        // dist , node:
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(a[0]-b[0]));
-        
-        pq.offer(new int[]{src , 0});
+        pq.offer(new int[]{0,src});
         
         while(!pq.isEmpty()){
             int[] curr = pq.poll();
-            
-            for(int[] ngbr : adj.get(curr[0])){
-                if(dist[ngbr[0]]>ngbr[1]+curr[1]){
-                    dist[ngbr[0]] = ngbr[1]+curr[1];
-                    pq.offer(new int[]{ngbr[0],dist[ngbr[0]]});
+            int currD = curr[0];
+            for(int[] ngbr : adj.get(curr[1])){
+                if(dist[ngbr[0]]>currD+ngbr[1]){
+                    dist[ngbr[0]] = currD+ngbr[1];
+                    pq.offer(new int[]{dist[ngbr[0]],ngbr[0]});
                 }
             }
         }
-        
         for(int i=0;i<V;i++){
             if(dist[i]==100001){
                 dist[i] = -1;
@@ -35,5 +36,6 @@ class Solution {
         }
         
         return dist;
+        
     }
 }
