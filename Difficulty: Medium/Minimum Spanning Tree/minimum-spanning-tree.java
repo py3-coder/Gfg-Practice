d739276a2a47ec29f4433806cad8716fa1bff919
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2026-03-21 13:22:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -1,66 +1,39 @@
 class Solution {
-    static int spanningTree(int V, int E, List<List<int[]>> adj) {
-        // Code Here.
-        DisjointSet ds = new DisjointSet(V);
-        List<int[]> edges = new ArrayList<>();
+    public int spanningTree(int V, int[][] edges) {
+        // code here
+        // Prims algo :
+        List<List<int[]>> adj = new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
         
-        for (int u = 0; u < V; u++) {
-            for (int[] nei : adj.get(u)) {
-                int v = nei[0], wt = nei[1];
-                if (u < v)
-                    edges.add(new int[]{u, v, wt});
+        for(int[] edge : edges){
+            int u = edge[0] , v = edge[1] , d = edge[2];
+            adj.get(u).add(new int[]{v,d});
+            adj.get(v).add(new int[]{u,d});
+        }
+        int[] vis = new int[V];
+        // wt , node , parent:
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(a[0]-b[0]));
+        // o wt  , oth node , -1 parent comming from beginning:
+        pq.offer(new int[]{0,0,-1});
+        List<int[]> mstEdges = new ArrayList<>();
+        int sum=0;
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            if(vis[curr[1]]==1) continue;
+            vis[curr[1]]=1;
+            if(curr[2]!=-1){
+                sum+=curr[0];
+                mstEdges.add(new int[]{curr[1],curr[2]});
+            }
+            for(int[] ngbr : adj.get(curr[1])){
+                int v = ngbr[0];
+                int d = ngbr[1];
+                pq.offer(new int[]{d,v,curr[1]});
             }
         }
-
-        edges.sort((a, b) -> (a[2] - b[2]));
-
-        int ans = 0;
-        for (int[] edge : edges) {
-            int u = edge[0], v = edge[1], wt = edge[2];
-            if (ds.findPar(u) != ds.findPar(v)) {
-                ans += wt;
-                ds.unionBySize(u, v);
-            }
-        }
-        return ans;
-    }
-}
-class DisjointSet{
-    List<Integer> parent;
-    List<Integer> size;
-    
-    public DisjointSet(int n){
-         parent = new ArrayList<>(n);
-         size = new ArrayList<>(n);
-         
-         for(int i=0;i<n;i++){
-            parent.add(i);
-            size.add(1);
-         }
-    }
-    
-    public int findPar(int node){
-        if(parent.get(node) == node) return node;
-         
-        int par = findPar(parent.get(node));
-        parent.set(node,par);
-        return parent.get(node);
-    }
         
-    public void unionBySize(int u,int v){
-        int ulpu = findPar(u);
-        int ulpv = findPar(v);
-        
-        if(ulpu == ulpv) return ;
-        
-        if(size.get(ulpu)>size.get(ulpv)){
-            parent.set(ulpv,ulpu);
-            size.set(ulpu , size.get(ulpu)+size.get(ulpv));
-        }else{
-            parent.set(ulpu,ulpv);
-            size.set(ulpv , size.get(ulpu)+size.get(ulpv));
-        }
+        return sum;
     }
-        
-    
 }
