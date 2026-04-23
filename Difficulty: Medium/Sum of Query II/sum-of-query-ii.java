c168e5254d783d3412136224155d4919da2adf43
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2026-04-23 15:30:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sum of Query II/sum-of-query-ii.java
+++ Difficulty: Medium/Sum of Query II/sum-of-query-ii.java
@@ -1,102 +1,54 @@
-//{ Driver Code Starts
-//Initial Template for Java
+// User function Template for Java
 
-import java.io.*;
-import java.util.*;
-import java.lang.*;
-
-class GFG{
-    static class FastReader{ 
-		BufferedReader br; 
-		StringTokenizer st; 
-  
-		public FastReader(){ 
-			br = new BufferedReader(new InputStreamReader(System.in)); 
-		} 
-  
-		String next(){ 
-			while (st == null || !st.hasMoreElements()){ 
-				try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
-			} 
-			return st.nextToken(); 
-		} 
-  
-		String nextLine(){ 
-			String str = ""; 
-			try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
-			return str; 
-		} 
-		
-		int nextInt(){
-		    return Integer.parseInt(next());
-		}
-	}
-	
-    public static void main(String args[])throws IOException
-    {
-        FastReader sc = new FastReader();
-        int t = sc.nextInt();
-        while(t-- > 0){
-            int n = sc.nextInt();
-            int arr[] = new int[n];
-            for(int i = 0;i < n;i++)
-                arr[i] = sc.nextInt();
-            int q = sc.nextInt();
-            int queries[] = new int[2*q];
-            for(int i = 0;i < 2*q;i++)
-                queries[i] = sc.nextInt();
-            
-            Solution ob = new Solution();
-            List<Integer> ans = new ArrayList<Integer>();
-            ans = ob.querySum(n, arr, q, queries);
-            StringBuilder out = new StringBuilder();
-            for(int i = 0;i < ans.size();i++)
-                out.append(ans.get(i) + " ");
-            System.out.println(out);
-        }
-    }
-}
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-class Solution{
-    public static int[] segmentTree;
-    List<Integer> querySum(int n, int arr[], int q, int queries[])
-    {
-        List<Integer> result = new ArrayList<>();
-        segmentTree = new int[4*n+1];
-        buildTree(0,0,n-1,arr);
-        for(int i=0;i<2*q;i+=2){
-            int val = query(0,0,n-1,queries[i]-1,queries[i+1]-1);
-            result.add(val);
-        }
-        return result;
-    }
-    public static void buildTree(int i,int l,int r,int[] nums){
-        //base case :
-        if(l == r){
-            segmentTree[i] = nums[l];
-            return ;
-        }
-        int mid = l+(r-l)/2;
-        buildTree(2*i+1,l,mid,nums);
-        buildTree(2*i+2,mid+1,r,nums);
+class Solution {
+    public int[] st;
+    List<Integer> querySum(int n, int arr[], int q, int queries[]) {
+        // code here
+        st = new int[4*n-1];
+        build(0,0,n-1,arr);
         
-        segmentTree[i] = segmentTree[2*i+1]+segmentTree[2*i+2];
+        List<Integer> result = new ArrayList<>();
+        
+        for(int i=0;i<queries.length;i+=2){
+            int a = queries[i];
+            int b = queries[i+1];
+            
+            
+            int res = query(0,0,n-1,a-1,b-1);
+            
+            result.add(res);
+        }
+        
+        return result;
+        
+    }
+    private void build(int i, int l, int r,int[] arr){
+        //base case:
+        if(l == r){
+            st[i] = arr[l];
+            return;
+        }
+        
+        int mid = l+(r-l)/2;
+        build(2*i+1,l,mid,arr);
+        build(2*i+2,mid+1,r,arr);
+        
+        st[i] = st[2*i+1] + st[2*i+2];
     }
     
-    public static int query(int i,int l,int r,int start,int end){
-        //Case 1: out of scope :
+    private int query(int i, int l, int r, int start, int end){
+        // 3 case:
+        
         if(l>end || r<start){
             return 0;
-        } // case 2: entire in range:
-        if(l>=start && end>=r){
-            return segmentTree[i];
+        }else if( l>=start && r<=end){
+            return st[i];
+        }else{
+            int mid = l+(r-l)/2;
+            return query(2*i+1,l,mid,start,end) + query(2*i+2,mid+1,r,start,end);
         }
-        //case 3: overlapping :
-        int mid = l+(r-l)/2;
-        return query(2*i+1,l,mid,start,end) + query(2*i+2,mid+1,r,start,end);
+        
     }
+    
+    
 }
